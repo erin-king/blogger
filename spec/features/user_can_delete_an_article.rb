a003a973e91e3475fcec45f37e1d58f08b0ad398
Erin King <44042415+erin-king@users.noreply.github.com>
2019-03-26 06:58:27 -0600

    Add test for deleting an article

---
+++ spec/features/user_can_delete_an_article.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "a user deletes an article" do
+  describe "they link from the show page" do
+    it "displays all articles execpt the deleted entry" do
+      article_1 = Article.create(title: "Article 1", body: "Lots of words.")
+      article_2 = Article.create(title: "Article 2", body: "Lots of messed up words.")
+
+      visit article_path(article_1)
+
+      click_link "Delete"
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to_not have_content(article_1.title)
+      expect(page).to have_content(article_2.title)
+    end
+  end
+end
